f02a9de5e6bff0223420c3778e76314f0f5243c3
Omnibus <psd1390@gmail.com>
2020-08-06 23:43:37 +0900

    await, Promise의 resolve와 reject, 비동기 함수 실행 시 try ~ catch 사용법

--- async.js
+++ async.js
@@ -134,4 +134,57 @@ function d() {
 a()
     .then(() => b())
     .then(() => c())
-    .then(() => d())
+    .then(() => d());
+
+// await : Promise를 리턴하는 함수 앞에 사용 가능
+// → a라는 함수가 실행되는 동안 대기하고 나서 이후 코드가 실행됨
+// → async 함수 내에서만 작동함!!
+async function asyncFunction() {
+    await a()
+    await b()
+    await c()
+    await d()
+    console.log('done');
+}
+
+// reject : 비동기 코드에서 에러가 발생했을 때 실행
+function a() {
+    return new Promise((resolve, reject) => {
+        if (isError) {
+            reject('Error Message!');
+        }
+
+        setTimeout(() => {
+            console.log('a');
+            resolve('Done!');
+        }, 1000);
+    });
+}
+
+a()
+    .then((res) => {   // resolve가 실행될 때
+        console.log(res);
+    }) 
+    .catch((error) => {  // reject가 실행될 때
+        console.log(error);
+    })
+    .finally(() => {
+
+    });
+
+// Example
+async function asyncFunc() {
+    // async, await의 then(), catch()를 사용하려면 try ~ catch 구문을 사용해야 함
+    try {
+        const res = await a();
+        console.log(res);
+
+    } catch (error) {    // reject가 실행될 때
+        console.log(error);
+
+    } finally {
+        console.log('Done!');
+    }
+}    
+
+asyncFunc();
