9a9cfe696256caa405c579b8df7b1ca7007b1357
Omnibus <psd1390@gmail.com>
2020-08-09 00:00:49 +0900

    api로 조회한 각 영화 데이터를 vuetify card로 꾸미기

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,16 +1,30 @@
 <template>
-    <v-row>
+    <v-row
+        v-masonry
+        item-selector=".item"
+    >
         <v-col
             v-for="movie in movies"
             :key="movie.imdbID"
+            
+            v-masonry-tile
+            class="item"
+
             cols="12"
             lg="3"
             md="3"
             sm="6"
         >
-            <div>{{ movie.Title }}</div>
-            <div>{{ movie.Year }}</div>
-            <div>{{ movie.Poster }}</div>
+            <v-card>
+                <v-img
+                    :src="movie.Poster"
+                    :alt="movie.Title"
+                    height="300"
+                >
+                </v-img>
+                <v-card-title>{{ movie.Title }}</v-card-title>
+                <v-card-subtitle>{{ movie.Year }}</v-card-subtitle>
+            </v-card>
         </v-col>
     </v-row>
 </template>
