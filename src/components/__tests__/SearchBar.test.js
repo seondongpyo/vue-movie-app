d3e1f3230456b0e813aada4806ec7d8b04180ac6
Omnibus <psd1390@gmail.com>
2020-08-21 00:03:27 +0900

    로딩 애니메이션 바 테스트 코드 작성

--- src/components/__tests__/SearchBar.test.js
+++ src/components/__tests__/SearchBar.test.js
@@ -1,4 +1,4 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils';
+import { mount, createLocalVue } from '@vue/test-utils';
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import SearchBar from '../SearchBar';
@@ -13,7 +13,7 @@ describe('SearchBar Component Test', () => {
 
     beforeEach(() => {
         vuetify = new Vuetify();
-        wrapper = shallowMount(SearchBar, {
+        wrapper = mount(SearchBar, {
             localVue,
             vuetify,
             store
@@ -23,5 +23,13 @@ describe('SearchBar Component Test', () => {
     test('제목 입력 시 스토어 업데이트 확인', () => {
         wrapper.vm.title = 'lion';
         expect(wrapper.vm.title).toBe('lion');
+    });
+
+    test('로딩 중 아이콘 확인', async () => {
+        wrapper.vm.$store.commit('movie/updateState', {
+            loading: true
+        });
+        await wrapper.vm.$nextTick(); // 화면 갱신을 보장
+        expect(wrapper.find('.v-progress-circular').exists()).toBe(true);
     });
 });
