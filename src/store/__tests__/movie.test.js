503b5fe494d41b96af4621bbddc8a67ed9673f14
Omnibus <psd1390@gmail.com>
2020-08-22 22:19:30 +0900

    store 테스트 ver.2

--- src/store/__tests__/movie.test.js
+++ src/store/__tests__/movie.test.js
@@ -1,12 +1,27 @@
 import movie from '../movie';
 
 describe('movie store test', () => {
+    let store;
+
+    beforeEach(() => {
+        store = movie;
+        store.state = movie.state();
+
+        // this.$store.commit('Mutation's name', payload)
+        store.commit = function (name, payload) {
+            store.mutations[name](store.state, payload);
+        }
+    });
+
     test('state 업데이트', () => {
-        const state = movie.state();
-        movie.mutations.updateState(state, {
+        store.commit('updateState', {
+            title: 'Hello!',
+            movies: [1, 2, 3],
             loading: true
         });
 
-        expect(state.loading).toBe(true);
+        expect(store.state.title).toBe('Hello!');
+        expect(store.state.movies).toEqual([1, 2, 3]);
+        expect(store.state.loading).toBe(true);
     });
 });
